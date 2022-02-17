863ce0980fab741d2e1df0a22d6a72cfc130ed0b
Polygon65 <99875965+Polygon65@users.noreply.github.com>
2022-02-17 17:27:27 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,8 @@
+. lib/moduler.sh
+
+Bash.import: text_display/IO.ECHO text_display/colorama
+
+Tulis.strN "$(mode.bold: kuning)[$(mode.bold: merah)!$(mode.bold: kuning)]$(mode.bold: putih) updating...$(default.color)\n"
 cd ../
 rm -rf chicken_tools
 git clone https://github.com/Bayu12345677/chicken_tools/
